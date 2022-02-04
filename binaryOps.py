cb389663f548df029b2a0a0c872f7555f7ccb3cd
sashakistochka <sashakistochka@protonmail.com>
2022-02-05 01:20:23 +0300

    Примитивный калькулятор для бинарных операций с целыми числами

---
+++ binaryOps.py
@@ -0,0 +1,13 @@
+print("Добро пожаловать в консольный калькулятор!")
+
+operand1 = input("Введите первый операнд (только целое число): ")
+operand2 = input("Введите второй операнд (только целое число, кроме ноля, если собираетесь делить): ")
+
+op = input("Введите оператор (+, -, /, *): ")
+
+if op == "+": result = int(operand1) + int(operand2)
+if op == "-": result = int(operand1) - int(operand2)
+if op == "/": result = int(operand1) / int(operand2)
+if op == "*": result = int(operand1) * int(operand2)
+
+print("Результат вычисления: " + str(result))
